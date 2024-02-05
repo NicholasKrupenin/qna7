b3c242bfc3a6904a3a822f3e1084fe20e71478ea
nw <krupenin.nikolay@gmail.com>
2024-02-05 16:52:05 +0600

    merge 1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :subscriptions, dependent: :destroy
+  has_many :subscriptions, dependent: :destrda
 
   has_many :authorizations, dependent: :destroy
 
